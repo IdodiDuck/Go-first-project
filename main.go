d1825885e90f30e5179a39c1d24587c02c19b4d8
Ido Redid <Ido.redid@gmail.com>
2022-12-21 12:51:27 +0200

    Basic Calculator
    Added Functions add and subtract including documentations of the functions and clear explainations.

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
+// Basic calculator functions add and subtract
+
+//Importations of fmt and package main
 package main
 import "fmt"
 
-func main() {
-	fmt.Println("Hello World!")
+func add(num1 int16, num2 int16) {
+	//This function is adding  the first number to the second number or the oppisite
+	//Parameters:
+	//-----------
+	//param num1: type int16
+	//		The first number the function recieve, the number which is being added with the second number argument
+	//		Note: type of int16 --> 16 bytes, used for small numbers in range of around 100-200 numbers, not for larger numbers which requires more bytes.
 
+	//Param num2: type int16
+	//		The second number which is the adder of the first number
+	//		Note: type of int16 --> 16 bytes like above.
+
+	fmt.Println(num1 - num2)
+}
+
+func subtract(num1 int16, num2 int16) {
+	//This function is subtracting the first number in the second number
+	//Parameters:
+	//-----------
+	//param num1: type int16
+	//		The first number the function recieve, the number which is being subtracted in the second number argument
+	//		Note: type of int16 --> 16 bytes, used for small numbers in range of around 100-200 numbers, not for larger numbers which requires more bytes.
+
+	//Param num2: type int16
+	//		The second number which is the subtractor of the first number
+	//		Note: type of int16 --> 16 bytes like above.
+
+	fmt.Println(num1 - num2)
+}
+
+func main() { 
+	// The main function of the program, doesn't recieve any argument unlike the other functions
+	
 }
